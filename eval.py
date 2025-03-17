06bde20de2b1f2eee60445467be5b636cbd810fa
Ethan Latimer <ethanlatimer@hotmail.co.uk>
2025-03-17 18:43:51 +0000

    Fixed line that would select the same task for all selections
    Changes:
    - Fixed line that would select the same task for all selections
    - Extra docuemntation for selecting dataset evaluation

--- eval.py
+++ eval.py
@@ -19,50 +19,79 @@ python eval.py --resume ./deepfake.pth --dataroot ~/workspace/datasets/DeepFake_
 
 
 def setup_parser():
-    parser = argparse.ArgumentParser(description='Reproduce of multiple continual learning algorthms.')
-    parser.add_argument('--resume', type=str, default='', help='resume model')
-    parser.add_argument('--dataroot', type=str, default='/home/wangyabin/workspace/DeepFake_Data/CL_data/', help='data path')
-    parser.add_argument('--datatype', type=str, default='core50', help='data type')
-    parser.add_argument('--random_select', action='store_true', help='use random select')
-    parser.add_argument('--til', action='store_true', help='use groundtruth task identification')
+    parser = argparse.ArgumentParser(
+        description="Reproduce of multiple continual learning algorthms."
+    )
+    parser.add_argument("--resume", type=str, default="", help="resume model")
+    parser.add_argument(
+        "--dataroot",
+        type=str,
+        default="/home/wangyabin/workspace/DeepFake_Data/CL_data/",
+        help="data path",
+    )
+    parser.add_argument("--datatype", type=str, default="core50", help="data type")
+    parser.add_argument(
+        "--random_select", action="store_true", help="use random select"
+    )
+    parser.add_argument(
+        "--til", action="store_true", help="use groundtruth task identification"
+    )
     return parser
+
 
 class DummyDataset(Dataset):
     def __init__(self, data_path, data_type):
 
-        self.trsf = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
+        self.trsf = transforms.Compose(
+            [
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+                ),
+            ]
+        )
 
         images = []
         labels = []
         if data_type == "deepfake":
             subsets = ["gaugan", "biggan", "wild", "whichfaceisreal", "san"]
-            multiclass = [0,0,0,0,0]
+            multiclass = [0, 0, 0, 0, 0]
             for id, name in enumerate(subsets):
-                root_ = os.path.join(data_path, name, 'val')
+                root_ = os.path.join(data_path, name, "val")
                 # sub_classes = ['']
-                sub_classes = os.listdir(root_) if multiclass[id] else ['']
+                sub_classes = os.listdir(root_) if multiclass[id] else [""]
                 for cls in sub_classes:
-                    for imgname in os.listdir(os.path.join(root_, cls, '0_real')):
-                        images.append(os.path.join(root_, cls, '0_real', imgname))
+                    for imgname in os.listdir(os.path.join(root_, cls, "0_real")):
+                        images.append(os.path.join(root_, cls, "0_real", imgname))
                         labels.append(0 + 2 * id)
 
-                    for imgname in os.listdir(os.path.join(root_, cls, '1_fake')):
-                        images.append(os.path.join(root_, cls, '1_fake', imgname))
+                    for imgname in os.listdir(os.path.join(root_, cls, "1_fake")):
+                        images.append(os.path.join(root_, cls, "1_fake", imgname))
                         labels.append(1 + 2 * id)
         elif data_type == "domainnet":
             self.data_root = data_path
             self.image_list_root = self.data_root
-            self.domain_names = ["clipart","infograph","painting","quickdraw", "real","sketch",]
-            image_list_paths = [os.path.join(self.image_list_root, d + "_" + "test" + ".txt") for d in self.domain_names]
+            self.domain_names = [
+                "clipart",
+                "infograph",
+                "painting",
+                "quickdraw",
+                "real",
+                "sketch",
+            ]
+            image_list_paths = [
+                os.path.join(self.image_list_root, d + "_" + "test" + ".txt")
+                for d in self.domain_names
+            ]
             imgs = []
             for taskid, image_list_path in enumerate(image_list_paths):
                 image_list = open(image_list_path).readlines()
-                imgs += [(val.split()[0], int(val.split()[1])+taskid*345) for val in image_list]
+                imgs += [
+                    (val.split()[0], int(val.split()[1]) + taskid * 345)
+                    for val in image_list
+                ]
 
             for item in imgs:
                 images.append(os.path.join(self.data_root, item[0]))
@@ -74,7 +103,7 @@ class DummyDataset(Dataset):
         else:
             pass
 
-        assert len(images) == len(labels), 'Data size error!'
+        assert len(images) == len(labels), "Data size error!"
         self.images = images
         self.labels = labels
 
@@ -87,9 +116,10 @@ class DummyDataset(Dataset):
         return idx, image, label
 
     def pil_loader(self, path):
-        with open(path, 'rb') as f:
+        with open(path, "rb") as f:
             img = Image.open(f)
-            return img.convert('RGB')
+            return img.convert("RGB")
+
 
 args = setup_parser().parse_args()
 model = torch.load(args.resume)
@@ -98,15 +128,15 @@ model = model.to(device)
 test_dataset = DummyDataset(args.dataroot, args.datatype)
 test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False, num_workers=8)
 
-X,Y = [], []
+X, Y = [], []
 
 for id, task_centers in enumerate(model.all_keys):
     X.append(task_centers.detach().cpu().numpy())
-    Y.append(np.array([id]*len(task_centers)))
+    Y.append(np.array([id] * len(task_centers)))
 
-X = np.concatenate(X,0)
-Y = np.concatenate(Y,0)
-neigh = KNeighborsClassifier(n_neighbors=1, metric='l1')
+X = np.concatenate(X, 0)
+Y = np.concatenate(Y, 0)
+neigh = KNeighborsClassifier(n_neighbors=1, metric="l1")
 neigh.fit(X, Y)
 
 selectionsss = []
@@ -122,13 +152,13 @@ for _, (path, inputs, targets) in enumerate(test_loader):
         if args.random_select:
             selection = np.random.randint(0, Y.max(), selection.shape)
         if args.til:
-            selection = (targets/345).cpu().long().numpy()
-            # selection = (targets/50).cpu().long().numpy()
-            # selection = (targets/2).cpu().long().numpy()
+            selection = (targets / 2).cpu().long().numpy()  # For CDDB-Hard
+            # selection = (targets/50).cpu().long().numpy()     # For DomainNET
+            # selection = (targets / 345).cpu().long().numpy()  # For Core50
 
         selectionsss.extend(selection)
 
-        selection = torch.tensor(selection).to(device)*0
+        selection = torch.tensor(selection).to(device)
 
         outputs = model.interface(inputs, selection)
     predicts = torch.topk(outputs, k=2, dim=1, largest=True, sorted=True)[1]
@@ -137,12 +167,14 @@ for _, (path, inputs, targets) in enumerate(test_loader):
 
 y_pred = np.concatenate(y_pred)
 y_true = np.concatenate(y_true)
-print(sum(selectionsss==((y_true/345).astype(int)))/(len(y_true)))
-if args.datatype == 'deepfake':
+
+print(sum(selectionsss == ((y_true / 2).astype(int))) / (len(y_true)))  # For CDDB-Hard
+# print(sum(selectionsss == ((y_true / 50).astype(int))) / (len(y_true)))   # For DomainNET
+# print(sum(selectionsss == ((y_true / 345).astype(int))) / (len(y_true)))  # For Core50
+
+if args.datatype == "deepfake":
     print(accuracy_binary(y_pred.T[0], y_true))
-elif args.datatype == 'domainnet':
+elif args.datatype == "domainnet":
     print(accuracy_domain(y_pred.T[0], y_true))
-elif args.datatype == 'core50':
+elif args.datatype == "core50":
     print(accuracy_core50(y_pred.T[0], y_true))
-
-
